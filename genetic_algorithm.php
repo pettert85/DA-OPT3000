2f2110afa827721e084e1e018a2314ccc8634850
Petter Thorsen <petter.thorsen85@gmail.com>
2019-10-20 15:09:07 +0200

    small change only

--- genetic_algorithm.php
+++ genetic_algorithm.php
@@ -1,22 +1,22 @@
 <?php
 
-define('MAX_WEIGHT', 30); //Max weight of knapsack
+define('MAX_WEIGHT', 30); //Max weight of kknapsack
 
 //Initialising the weight and values of the objects to some random numbers.
 $object_weight = array(2,7,6,4,8,9,1,9,6,5);
 $object_value = array(1,73,36,89,9,19,50,62,38,99);
 
-// We  generate a population of four individuals first (knapsacks)
-$knapsacks = array(
-	create_napsack($object_weight),
-	create_napsack($object_weight),
-	create_napsack($object_weight),
-	create_napsack($object_weight));
+// We  generate a population of four individuals first (kknapsacks)
+$kknapsacks = array(
+	create_knapsack($object_weight),
+	create_knapsack($object_weight),
+	create_knapsack($object_weight),
+	create_knapsack($object_weight));
 
 
-$optimal_knapsack = genetic_algorithm($knapsacks,$object_value,$object_weight);
+$optimal_kknapsack = genetic_algorithm($kknapsacks,$object_value,$object_weight);
 
-printf("Optimized Napsack -> weight: %d value: %d\n", total_weight($optimal_knapsack, $object_weight), total_value($optimal_knapsack,$object_value)); 
+printf("Optimized knapsack -> weight: %d value: %d\n", total_weight($optimal_kknapsack, $object_weight), total_value($optimal_kknapsack,$object_value)); 
 
 
 function genetic_algorithm($population,$object_value,$object_weight){
@@ -122,7 +122,7 @@ function genetic_algorithm($population,$object_value,$object_weight){
 }
 
 
-function create_napsack($object_weight){
+function create_knapsack($object_weight){
 	$knapsack = array(0,0,0,0,0,0,0,0,0,0); //0's means item is not selected, and 1's mean selected
 	
 	$weight = 0;
@@ -132,7 +132,7 @@ function create_napsack($object_weight){
 				$weight += $object_weight[$i];
 
 				if($weight < MAX_WEIGHT){
-					$knapsack[$i] = 1;
+					$kknapsack[$i] = 1;
 				}
 			}
 		}
@@ -140,12 +140,12 @@ function create_napsack($object_weight){
 	return $knapsack;
 }
 
-function total_weight($knapsack, $object_weight){
-	//returns the total weight of the objects in the napsack given as argument one.
+function total_weight($kknapsack, $object_weight){
+	//returns the total weight of the objects in the knapsack given as argument one.
 	$weight = 0;
 
-	for($i = 0; $i < count($knapsack); $i++){
-		if($knapsack[$i] == 1 ){
+	for($i = 0; $i < count($kknapsack); $i++){
+		if($kknapsack[$i] == 1 ){
 			$weight += $object_weight[$i];
 		}
 	}
@@ -153,12 +153,12 @@ function total_weight($knapsack, $object_weight){
 	return $weight;
 }
 
-function total_value($knapsack, $object_value){
-	//returns the total value of the objects in the napsack given as argument one
+function total_value($kknapsack, $object_value){
+	//returns the total value of the objects in the knapsack given as argument one
 	$value = 0;
 
-	for($i = 0; $i < count($knapsack); $i++){
-		if($knapsack[$i] == 1 ){
+	for($i = 0; $i < count($kknapsack); $i++){
+		if($kknapsack[$i] == 1 ){
 			$value += $object_value[$i];
 		}
 	}
